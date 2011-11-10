727e9a481f129a62566f7bf1286f021b6973db67
alegch <chebulaev.oleg@gmail.com>
2011-11-10 16:27:03 +0300

    add files site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UlAction::Application.config.secret_token = '1a77567066ddf71df82ac651d34d5b4aa86a60eec04ab8ed9552ab24b91320c6e6cb371b571a222621ad2c5c4b7172250c74ac96cf841f455a1b2d77304cfa42'
